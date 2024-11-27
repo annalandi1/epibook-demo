2c39f65aee803dcb672bb5937edcba1dc65529d9
annalandi1 <landianna.1106@gmail.com>
2024-11-27 16:39:19 +0100

    update
    non capisco l'errore

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import NavComponent from './components/NavComponent';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavComponent from './components/NavComponent';
+import MyFooter from './components/MyFooter';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavComponent />
+        <NavComponent isFluid={true}/>
       </header>
+      <footer>
+        <MyFooter/>
+      </footer>
     </div>
   );
 }
