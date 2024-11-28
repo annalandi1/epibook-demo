caac796a21f7c7bafa0c85e7b07217599e3bda1b
annalandi1 <landianna.1106@gmail.com>
2024-11-28 16:05:37 +0100

    update

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
+import  Container  from 'react-bootstrap';
 
  const MyFooter = function () {
   return (
-    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#21081a' }}>
-      <MDBContainer className='p-4'></MDBContainer>
+    <div className='text-center text-white' style={{ backgroundColor: '#21081a' }}>
+      <Container className='p-4'></Container>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2024 Copyright:
           Epibook.org
         
       </div>
-    </MDBFooter>
+    </div>
   );
 }
 
